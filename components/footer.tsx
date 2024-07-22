1e76a7e0eb7388de686ee3cfb1b9157234b93e37
Saad Iqbal <saadiqbal.dev@outlook.com>
2024-07-22 21:41:31 +0500

    Footer Done

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,11 +14,11 @@ import NearLogo from "@/public/Near-landscape.svg";
 export default function Footer() {
   return (
     <footer className="bg-[#1B1F2A] text-[#BEBDBE]">
-      <div className="container mx-auto flex flex-col py-14">
-        <div className="flex flex-row items-center justify-between">
+      <div className="container mx-auto flex flex-col py-14 pl-6 md:py-14 md:pl-0 lg:py-20">
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between lg:flex-row">
           <div className="flex max-w-72 flex-col gap-4">
-            <h4 className="font-bold">Did we miss something?</h4>
-            <p className="text-[10px] font-medium">
+            <h4 className="text-sm font-bold">Did we miss something?</h4>
+            <p className="text-[12px] font-medium">
               Is your project also built on NEAR but you could not find it in
               our Landscape? Submit your project and we will review it shortly.
             </p>
@@ -26,8 +26,8 @@ export default function Footer() {
               Submit your project
             </GradientButton>
           </div>
-          <div className="flex flex-col justify-end gap-7">
-            <div className="flex items-center gap-5">
+          <div className="flex flex-col justify-end gap-7 pt-8 md:pt-0 lg:pt-0">
+            <div className="flex flex-col items-start gap-2 md:flex-row md:items-center md:gap-5 lg:flex-row lg:items-center lg:gap-5">
               <Link href="https://near.org" target="_blank">
                 near.org
               </Link>
@@ -38,7 +38,7 @@ export default function Footer() {
                 near.foundation
               </Link>
             </div>
-            <div className="flex items-center justify-end gap-5">
+            <div className="flex gap-5 md:items-center md:justify-end">
               <Link
                 className="transition-opacity duration-300 ease-in-out hover:opacity-50"
                 href="#"
@@ -82,7 +82,7 @@ export default function Footer() {
                 <Image src={Youtube} alt="Youtube" width={32} height={32} />
               </Link>
             </div>
-            <div className="flex items-center justify-end gap-5">
+            <div className="flex gap-5 md:items-center md:justify-end lg:items-center lg:justify-end">
               <Link href="#" target="_blank">
                 Cookie Policy
               </Link>
@@ -92,11 +92,11 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center gap-5">
+        <div className="flex flex-col gap-5 pt-10 md:items-center md:justify-center md:pt-0 lg:pt-0">
           <Link href="#" target="_blank">
             <Image src={NearLogo} alt="Near Logo" />
           </Link>
-          <p className="font-extrabold">
+          <p className="w-60 font-medium md:w-fit md:font-extrabold">
             Built with ❤️ by Banyan, NEAR Balkans and NEAR Catalog 🚀{" "}
           </p>
         </div>
