a36bbfc21f4cc7550be92e9d83782a282568db32
Zeeshan Ahmad <itexpert120@outlook.com>
2024-07-26 12:25:26 +0500

    Improve Tags mobile view

--- store/search-store.ts
+++ store/search-store.ts
@@ -3,6 +3,8 @@ import { create } from "zustand";
 interface SearchStore {
   tags: string[];
   setTags: (tags: string[]) => void;
+  allTags: string[];
+  setAllTags: (allTags: string[]) => void;
   searchKey: string;
   setSearchKey: (searchKey: string) => void;
 }
@@ -10,6 +12,8 @@ interface SearchStore {
 export const useSearchStore = create<SearchStore>((set) => ({
   tags: [],
   setTags: (tags) => set({ tags }),
+  allTags: [],
+  setAllTags: (allTags) => set({ allTags }),
   searchKey: "",
   setSearchKey: (searchKey) => set({ searchKey }),
 }));
