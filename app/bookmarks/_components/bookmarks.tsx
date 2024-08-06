1f284338999ef0d97569ce3bd8a6205e6a93365c
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-08-06 10:54:19 -0500

    organizes and test bookmarking

--- app/bookmarks/_components/bookmarks.tsx
+++ app/bookmarks/_components/bookmarks.tsx
@@ -3,20 +3,16 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { fetchAllProjects } from "@/lib/near-catalog";
-import { ProjectRecord } from "@/lib/types";
+import { ProjectId, ProjectRecord } from "@/lib/types";
 import ProjectCard from "@/components/ui/project-card";
+import { getBookmarkedProjects } from "@/lib/bookmark-project";
 
 export default function Bookmarks() {
   const [projects, setProjects] = useState<Record<string, ProjectRecord>>();
-  const [starredProjects, setStarredProjects] = useState<string[]>([]);
+  const starredProjects = getBookmarkedProjects();
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    let starredProjectsString = localStorage.getItem("starredProjects");
-    if (starredProjectsString) {
-      setStarredProjects(JSON.parse(starredProjectsString));
-    }
-
     const fetchProjects = async () => {
       setLoading(true);
       const projects = await fetchAllProjects();
@@ -28,7 +24,7 @@ export default function Bookmarks() {
 
   return (
     <>
-      {starredProjects.length === 0 && (
+      {starredProjects.length === 0 ? (
         <div className="my-32 flex flex-col items-center justify-center gap-4 font-medium text-[#BEBDBE]">
           <Image
             src={"/assets/images/error.webp"}
@@ -38,21 +34,20 @@ export default function Bookmarks() {
           />
           <h2>No bookmarked projects found</h2>
         </div>
-      )}
-      {loading || !projects ? (
-        <div className="my-32 flex flex-col items-center justify-center gap-4 font-medium text-[#BEBDBE]">
-          <i className="bi bi-arrow-repeat animate-spin text-2xl"></i>
-        </div>
       ) : (
-        <div className="mt-4 grid max-w-full grid-cols-1 place-items-center items-stretch gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {starredProjects.map((projectSlug) => (
-            <ProjectCard
-              key={projectSlug}
-              project={projects[projectSlug]}
-              maxWidth
-            />
-          ))}
-        </div>
+        <>
+          {loading || !projects ? (
+            <div className="my-32 flex flex-col items-center justify-center gap-4 font-medium text-[#BEBDBE]">
+              <i className="bi bi-arrow-repeat animate-spin text-2xl"></i>
+            </div>
+          ) : (
+            <div className="mt-4 grid max-w-full grid-cols-1 place-items-center items-stretch gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              {starredProjects.map((pid: ProjectId) => (
+                <ProjectCard key={pid} project={projects[pid]} maxWidth />
+              ))}
+            </div>
+          )}
+        </>
       )}
     </>
   );
