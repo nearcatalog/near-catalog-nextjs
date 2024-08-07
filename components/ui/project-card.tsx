54d2f00e668060accfb5b307ae115d56d1de591e
Zeeshan Ahmad <itexpert120@outlook.com>
2024-08-07 16:46:17 +0500

    improve mobile view for project card

--- components/ui/project-card.tsx
+++ components/ui/project-card.tsx
@@ -41,16 +41,16 @@ export default function ProjectCard({ project, maxWidth }: ProjectCardProps) {
       }}
       className={`grow-1 min-h-92 flex w-full ${maxWidth ? "" : "max-w-[20rem]"} gap- shrink-0 cursor-pointer flex-col items-start justify-start gap-3 overflow-hidden rounded-lg bg-[#11141B] px-8 py-7 transition-all duration-300 ease-in-out hover:bg-[#2b2d3a] md:justify-normal`}
     >
-      <div className="flex h-full w-full items-center gap-2 overflow-hidden md:h-auto md:flex-col md:items-start">
+      <div className="flex w-full items-center gap-2 overflow-hidden md:h-auto md:flex-col md:items-start">
         <Image
-          className="size-[6rem] rounded-full md:size-[6rem]"
+          className="size-[4rem] rounded-full md:size-[6rem]"
           src={profile.image.url}
           alt={profile.name}
           width={120}
           height={120}
         />
         <div className="flex flex-col gap-1">
-          <h3 className="m-0 overflow-ellipsis break-words p-0 text-[1.25rem] font-bold text-white md:break-words">
+          <h3 className="m-0 overflow-ellipsis break-words p-0 text-[1.25rem] font-bold leading-tight text-white md:break-words">
             {title}
           </h3>
         </div>
