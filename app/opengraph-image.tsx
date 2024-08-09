87f7c39aa3b305a75379a7b20be22d7f37e9c842
Zeeshan Ahmad <itexpert120@outlook.com>
2024-08-09 15:12:26 +0500

    Fix og image alt

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -5,7 +5,7 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 // Image metadata
-export const alt = "About Acme";
+export const alt = "NEAR Catalog";
 export const size = {
   width: 1200,
   height: 630,
