1537d891f4ea8a608ddc3df3f3a7931c2bb93e43
Zeeshan Ahmad <itexpert120@outlook.com>
2024-08-09 12:37:01 +0500

    Fix padding

--- app/policy/layout.tsx
+++ app/policy/layout.tsx
@@ -4,7 +4,7 @@ export default function PolicyLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="container mx-auto my-4 flex flex-col gap-4 p-4">
+    <div className="container mx-auto my-4 p-4">
       <div className="prose prose-invert max-w-none">{children}</div>
     </div>
   );
