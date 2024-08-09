5938b208c90a04dfac7f0947a5aa0c854a957659
Zeeshan Ahmad <itexpert120@outlook.com>
2024-08-09 17:19:09 +0500

    Fix tests

--- app/project/[pid]/_components/tags.tsx
+++ app/project/[pid]/_components/tags.tsx
@@ -8,7 +8,7 @@ export default function Tags({
   handleTagClick?: () => void;
 }) {
   return (
-    <div className="flex flex-wrap items-center gap-2">
+    <div className="tags flex flex-wrap items-center gap-2">
       {Object.keys(tags).map((key, index) => (
         <Link
           href={`/category/${key}#top`}
